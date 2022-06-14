03e51dfa3eeaab36d7024b8bdbcd87b9506e0539
zhuhaoran <zhuhaoran@ambition.com>
2022-06-14 11:21:25 +0800

    侧边栏

---
+++ views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <el-container style="height:400px">
+  <el-aside width="auto">
+    <CommonAside></CommonAside>
+  </el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</div>
+</template>
+
+<script>
+
+import CommonAside from '../src/components/CommonAside.vue'
+export default {
+     name:'Home',
+     components:{
+      CommonAside,
+     },
+     data(){
+        return{
+
+        }
+     }
+   
+}
+</script>
+
+<style lang="less" scoped>
+.el-header{
+   background-color: #333;
+
+}
+.el-main{
+   padding-top:0;
+}
+
+</style>
---
+++ views/User.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>我是user页面</div>
+</template>
+
+<script>
+export default {
+     name:'User',
+     data(){
+        return{
+
+        }
+     }
+}
+</script>
+
+<style>
+
+</style>
